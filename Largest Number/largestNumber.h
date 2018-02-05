55ef98a8b81e80f089cfd5697473e0006ac96a43
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-02-05 13:02:51 -0800

    Having issues

---
+++ Largest Number/largestNumber.h
@@ -0,0 +1,15 @@
+//
+//  largestNumber.h
+//  Largest Number
+//
+//  Created by Chris Dean on 2018-02-05.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface largestNumber : NSObject
+
++(NSNumber*)unsorted(NSArray*) numbers;
+
+@end
---
+++ Largest Number/largestNumber.m
@@ -0,0 +1,23 @@
+//
+//  largestNumber.m
+//  Largest Number
+//
+//  Created by Chris Dean on 2018-02-05.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import "largestNumber.h"
+
+@implementation largestNumber
+
++(NSNumber*)unsorted:(NSArray*) numbers  {
+    
+    //NSArray arr = @[ @2, @144, @11, @99, @35 ];
+    NSNumber *maxNumber = [numbers valueForKeyPath:@"@max.self"];
+    NSLog(@"%@", maxNumber);
+    
+    
+    return maxNumber;
+}
+
+@end
